f3d50ef77bdb12877916c7b5dc2d9f1fb2800ab5
KnownUsername <joaoftrodrigues@gmail.com>
2021-11-08 11:26:20 +0000

    Created base for lexer interpreter

---
+++ country_lexer.py
@@ -0,0 +1,35 @@
+from country import Country
+
+class CountryLexer:
+
+    """ ! Check if the variables are kept in class, or outside !"""
+
+    # List of all countries
+    fields = Country.fields
+    field_index = 0
+
+    current_country = Country()
+    countries = []
+
+
+
+    def t_NEWLINE(self, t):
+        r'\n'
+
+        # Restart index value
+        self.field_index = 0
+
+        # Store read country
+
+        return t
+
+    def t_FIELD(self, t):
+        r'[^,]'
+        #global field_index
+
+        # Set value of a class attribute ( Something close to this -> current_country.fields[field_index] = t.value )
+        setattr(self.current_country, self.fields[self.field_index], t.value)
+
+        self.field_index += 1  # Index increment, to change column/field
+        return t
+
