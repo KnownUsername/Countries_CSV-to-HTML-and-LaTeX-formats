142005e54943791d88cb903ffd19c289a96e558f
KnownUsername <joaoftrodrigues@gmail.com>
2021-11-08 12:36:38 +0000

    Added feature to add countries to a list and clear aux variable, after each country.

--- country_lexer.py
+++ country_lexer.py
@@ -5,7 +5,6 @@ class CountryLexer:
     """ ! Check if the variables are kept in class, or outside !"""
 
     # List of all countries
-    fields = Country.fields
     field_index = 0
 
     current_country = Country()
@@ -19,16 +18,18 @@ class CountryLexer:
         self.field_index = 0
 
         # Store read country
+        self.countries.append(self.current_country)
 
+        # Set all class's variables to None
+        self.countries.clear()
 
-        return t
+        return t  # does it return?
 
     def t_FIELD(self, t):
         r'[^,]'
 
         # Set value of a class attribute ( Something close to this -> current_country.fields[field_index] = t.value )
-        setattr(self.current_country, self.fields[self.field_index], t.value)
+        setattr(self.current_country, Country.fields[self.field_index], t.value)
 
         self.field_index += 1  # Index increment, to change column/field
         return t
-
