a9417d8a65eb92c72d2dccbbcf12fd720bc1b793
KnownUsername <joaoftrodrigues@gmail.com>
2021-11-09 15:23:19 +0000

    First version of storing file data into objects. Last line not being stored.

--- country_lexer.py
+++ country_lexer.py
@@ -1,3 +1,5 @@
+import copy
+
 from country import Country
 from my_utils import slurp
 import ply.lex as plex
@@ -5,7 +7,7 @@ import ply.lex as plex
 class CountryLexer:
 
     """ ! Check if the variables are kept in class, or outside !"""
-    tokens = ("NEWLINE", "FIELD", "COMMA")
+    tokens = ("NEWLINE", "NFIELD", "QMFIELD", "COMMA")
 
     # List of all countries
     column_index = 0
@@ -13,29 +15,58 @@ class CountryLexer:
     current_country = Country()
     countries = []
 
+    def t_NFIELD(self, t):
+        r'[a-z A-Z-\'£Ã§]+'
+
+        # Set value of a class attribute ( Something close to this -> current_country.fields[field_index] = t.value )
+        setattr(self.current_country, Country.columns[self.column_index], t.value)
+
+        #print(t.value)
+        #print(self.current_country.country_name)
+
+        #self.field_index += 1  # Index increment, to change column/field
+        return t
+
+    def t_QMFIELD(self, t):
+        r'"[^"]+"'
+
+        print(t.value)
+        # Set value of a class attribute ( Something close to this -> current_country.fields[field_index] = t.value )
+        setattr(self.current_country, Country.columns[self.column_index], t.value)
+
+        return t
+
+    def t_COMMA(self, t):
+        r','
+        #print(self.column_index)
+        self.column_index += 1  # Index increment, to change column/field
+        return t
 
     def t_NEWLINE(self, t):
         r'\n'
-        print(t.value)
+        #print(self.current_country.country_name)
+        #print(t.value)
         # Restart index value
-        self.field_index = 0
+        self.column_index = 0
 
         # Store read country
-        self.countries.append(self.current_country)
+        self.countries.append(copy.deepcopy(self.current_country))
 
         # Set all class's variables to None
-        self.countries.clear()
+        self.current_country.clean()
 
         return t  # does it return?
 
-    def t_FIELD(self, t):
-        r'[^,]'
-        #print(t.value)
-        # Set value of a class attribute ( Something close to this -> current_country.fields[field_index] = t.value )
-        setattr(self.current_country, Country.columns[self.field_index], t.value)
+    #def t_EOF(self, t):
+     #   r'\z'
 
-        self.field_index += 1  # Index increment, to change column/field
-        return t
+        # Store read country
+      #  self.countries.append(copy.deepcopy(self.current_country))
+
+        # Set all class's variables to None
+       # self.current_country.clean()
+
+        #return t
 
     def t_error(self, t):
         print(f"Unexpected tokens: {t.value[0:10]}")
@@ -57,6 +88,21 @@ cl.initialize()
 #cl.lexer = plex.lex()
 cl.lexer.input(file)
 for token in iter(cl.lexer.token, None):
+    #print(token.value)
     pass
 print(cl.countries)
+for country in cl.countries:
+    country.present()
+
 print("Finished processing")
+#print(cl.current_country.country_name)
+#print(cl.current_country.country_name)
+#teste = CountryLexer()
+#setattr(teste.current_country, Country.columns[0], 'Portugal')
+#print(teste.current_country.country_name)
+#setattr(teste.current_country, Country.columns[0], 'Brazil')
+#print(teste.current_country.country_name)
+
+
+
+
