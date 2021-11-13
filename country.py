2a5c971ab82fad7ace122a38d32c420fe591f04f
KnownUsername <joaoftrodrigues@gmail.com>
2021-11-13 18:42:43 +0000

    Removed attributes from Country class. Adapted methods to be used independently of the elements of columns' list.

--- country.py
+++ country.py
@@ -3,23 +3,14 @@
 class Country:
 
     # Contains all fields' name (each column name)
-    columns = ['country_name', 'capital', 'currency', 'official_languages', 'head_of_government']
-
-    def __init__(self, country_name = None, capital = None, currency = None, official_languages = None, head_of_government = None):
-        self.country_name = country_name
-        self.capital = capital
-        self.currency = currency
-        self.official_languages = official_languages
-        self.head_of_government = head_of_government
+    columns = []
 
     """  Changes the value of every field into None. 
     This can be used, in order to reutilize a variable"""
     def clean(self):
-        self.country_name = None
-        self.capital = None
-        self.currency = None
-        self.official_languages = None
-        self.head_of_government = None
+        for column in self.columns:
+            setattr(self, column, None)
 
     def present(self):
-        print(f'{self.country_name}, {self.capital}, {self.currency}, {self.official_languages}, {self.head_of_government}')
+        for column in self.columns:
+            getattr(self, column)
