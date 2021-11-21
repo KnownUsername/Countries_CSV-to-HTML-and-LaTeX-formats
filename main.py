04dc8977ef99999bf4775ba522fd8e0e04beb520
KnownUsername <joaoftrodrigues@gmail.com>
2021-11-21 18:14:48 +0000

    Removed debug code; Updated methods; Added and updated comments.

--- main.py
+++ main.py
@@ -1,18 +1,12 @@
 from country_lexer import CountryLexer
 from latex_generator import Latex_Generator
 
-# Instance of class
-cl = CountryLexer('list1.csv')
-
-# Apply processes on file
+# Instance of class to apply Language Processing
+cl = CountryLexer('list1_new.csv')
+# Apply lex processes on file
 cl.process()
 
-# Show countries stored on list
-for country in cl.countries:
-    country.present()
-
-print("Finished processing")
-
+# Instance of class to build LaTeX file
 lg = Latex_Generator('countries.tex', countries = cl.countries)
-lg.initialize_doc()
-lg.datafile_fill()
+# Writes info into a file with given name on class builder
+lg.auto_fill_file()
