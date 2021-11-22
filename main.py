63226df43f81455ab14bb7575908ce1aa446f714
KnownUsername <joaoftrodrigues@gmail.com>
2021-11-22 02:48:41 +0000

    Added HTML feature

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from country_lexer import CountryLexer
 from latex_generator import Latex_Generator
+from html_generator import HTML_Generator
 
 # Instance of class to apply Language Processing
 cl = CountryLexer('list1_new.csv')
@@ -10,3 +11,6 @@ cl.process()
 lg = Latex_Generator('countries.tex', countries = cl.countries)
 # Writes info into a file with given name on class builder
 lg.auto_fill_file()
+
+html = HTML_Generator('countries.html', countries = cl.countries)
+html.auto_fill_file()
