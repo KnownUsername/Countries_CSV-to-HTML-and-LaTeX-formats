6fe478db543844972b8064714f2ce30cf6b87480
KnownUsername <joaoftrodrigues@gmail.com>
2021-11-23 16:06:49 +0000

    Restructured code; Added and edited comments

--- country_lexer.py
+++ country_lexer.py
@@ -14,7 +14,7 @@
 
 # Imports
 from country import Country
-from my_utils import slurp, split_columns_values
+from my_utils import slurp
 import ply.lex as plex
 import copy
 
@@ -32,16 +32,68 @@ class CountryLexer:
         ("comment", "exclusive")
     )
 
-    # List of all countries
-    column_index = 0
 
-    # Current country being read
-    current_country = Country()
+    def __init__(self, filename):
+        """ Class constructor.
 
-    # All countries read
-    countries = []
+            It's assigned it filename and initialized lexer variable with None value.
+
+            It's also initialized lists and auxiliar variables.
+            Info for each variable, available on comment.
+        """
+
+        self.lexer = None
+        self.filename = filename
+
+        # Stores the state that precedes 'comment' state
+        self.previous_state = None
+
+        # Index of current column/field
+        self.column_index = 0
+
+        # Current country being read
+        self.current_country = Country()
+
+            # -  Data Structures - #
+
+        # Comments found
+        self.comments = []
+
+        # All countries read
+        self.countries = []
+
+    def process(self):
+        """ Processes a csv file.
+                - Reads it
+                - Applies tokens' rules
+                - Saves elements on a list (each line is an element)
+        """
+
+        # Reads a file content and saves it as string
+        file = slurp(self.filename)
+
+        # Initializes lexer variable
+        self.lexer = plex.lex(module=self)
+
+        # Introduces file's content on lexer
+        self.lexer.input(file)
+        # Start lexer on header state
+        self.lexer.begin('header')
+
+        # Tokens' processing
+        for token in iter(self.lexer.token, None):
+            pass
+
 
     #   --- Token Rules ---   #
+
+    # Error manager
+    def t_ANY_error(self, t):
+        print(f"Unexpected tokens: {t.value[0:10]}")
+        exit(1)
+
+
+    #     ==> INITIAL <==
     def t_DUMMY(self, t):
         """ It's only defined, so that state INITIAL has a method associated to it.
 
@@ -53,89 +105,108 @@ class CountryLexer:
         r'[-\w \']+'
         pass
 
+    #   ###################   #
+
+    #     ==>    HEADER    <==
+
+    # Tokens starting with #
     def t_header_HASHTAG(self, t):
         r"\#[^,^\n]+"
 
+        # Check if '#' is the 1st character on line
+        # This occurs when column_index is 0, as it would be 1st field
         if self.column_index == 0:
-            print(t.value + "\n")
             self.comments.append(t.value)
+
+            # Assign previous_state, so comment knows for where to return
             self.previous_state = 'header'
+
+            # Change to comment state, to catch missing string on comment
             self.lexer.begin('comment')
 
         else:
-            print("Viva " + t.value)
+            # Add value to list of columns
             self.current_country.columns.append(t.value)
 
-
+    # Normal fields - No quotation marks
     def t_header_NFIELD(self, t):
         r'[-\w ()\'#]+'
+
+        # Add value to list of columns
         self.current_country.columns.append(t.value)
 
 
     # Fields containing quotation marks
     def t_header_QMFIELD(self, t):
         r'"[^"]+"'
+
+        # Add value to list of columns
         self.current_country.columns.append(t.value)
 
     # Commas
     def t_header_COMMA(self, t):
         r','
-        self.column_index += 1  # Index increment, to change column/field
+
+        # Index increment, to change column/field
+        self.column_index += 1
 
     # New lines
     def t_header_NEWLINE(self, t):
         r'\n'
+
+        # Reinitialize variable
         self.column_index = 0
+
+        # Change to body state
         self.lexer.begin('body')
 
+    #   ###################   #
 
-    # Normal fields - No quotation marks
+    #    ==>    BODY    <==
 
+    # Tokens starting with #
     def t_body_HASHTAG(self, t):
         r"\#[^,^\n]+"
 
+        # Check if '#' is the 1st character on line
+        # This occurs when column_index is 0, as it would be 1st field
         if self.column_index == 0:
             self.comments.append(t.value)
+
+            # Assign previous_state, so comment knows for where to return
             self.previous_state = 'body'
+
+            # Change to comment state, to catch missing string on comment
             self.lexer.begin('comment')
 
         else:
-            # Set value of a class attribute ( Something close to this -> current_country.fields[field_index] = t.value )
+            # Set value of a class attribute
+            # ( Something close to this -> current_country.columns[column_index] = t.value )
             setattr(self.current_country, Country.columns[self.column_index], t.value)
 
+    # Normal fields - No quotation marks
     def t_body_NFIELD(self, t):
         r'[-\w &()\'\.#]+'
 
-        # Set value of a class attribute ( Something close to this -> current_country.fields[field_index] = t.value )
+        # Set value of a class attribute
+        # ( Something close to this -> current_country.columns[column_index] = t.value )
         setattr(self.current_country, Country.columns[self.column_index], t.value)
-
-
-    def t_comment_FILL(self, t):
-        r"[^\n]+"
-
-        self.comments[-1] = self.comments[-1] + t.value
-
-    def t_comment_NEWLINE(self, t):
-        r"\n"
-        print("Opo")
-        # returns to previous state
-        self.lexer.begin(self.previous_state)
-
-
 
     # Fields containing quotation marks
     def t_body_QMFIELD(self, t):
         r'"[^"]+"'
-        modified_token = t.value
-        modified_token = modified_token[1:-1]
-        # Set value of a class attribute ( Something close to this -> current_country.fields[field_index] = t.value )
+
+        # Remove " " on start and end of string
+        modified_token = t.value[1:-1]
+        # Set value of a class attribute
+        # ( Something close to this -> current_country.columns[column_index] = t.value )
         setattr(self.current_country, Country.columns[self.column_index], modified_token)
 
     # Commas
     def t_body_COMMA(self, t):
         r','
-
-        self.column_index += 1  # Index increment, to change column/field
+        # Index increment, to change column/field
+        self.column_index += 1
 
     # New lines
     def t_body_NEWLINE(self, t):
@@ -159,44 +230,21 @@ class CountryLexer:
         # Set all class's variables to None
         self.current_country.clean()
 
-    # Error manager
-    def t_ANY_error(self, t):
-        print(f"Unexpected tokens: {t.value[0:10]}")
-        exit(1)
 
-    #   ###################   #
+    #    ==>    COMMENT    <==
 
+    # Matches text on same line as found '#' character on a line comment.
+    # Matched text is concatenated to precede string
+    def t_comment_FILL(self, t):
+        r"[^\n]+"
 
-    def __init__(self, filename):
-        """ Class constructor.
+        # Concatenate rest of line to first string stored
+        # before change to comment state
+        self.comments[-1] = self.comments[-1] + t.value
 
-            It's assigned it filename and initialized lexer variable with None value.
+    # New lines
+    def t_comment_NEWLINE(self, t):
+        r"\n"
 
-        """
-
-        self.lexer = None
-        self.filename = filename
-        self.previous_state = None
-        self.comments = []
-
-    def process(self):
-        """ Processes a csv file.
-                - Reads it
-                - Applies tokens' rules
-                - Saves elements on a list (each line is an element)
-        """
-
-        # Reads a file content and saves it as string
-        file = slurp(self.filename)
-
-        # Initializes lexer variable
-        self.lexer = plex.lex(module=self)
-
-        # Introduces file's content on lexer
-        self.lexer.input(file)
-        # Start lexer on header state
-        self.lexer.begin('header')
-
-        # Tokens' processing
-        for token in iter(self.lexer.token, None):
-            pass
+        # Returns to previous state
+        self.lexer.begin(self.previous_state)
