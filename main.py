b2c79c93d72e39694d114b4d13e32b1fa1752faf
KnownUsername <joaoftrodrigues@gmail.com>
2021-11-25 15:29:05 +0000

    Added feature to pass filename via command line, on running program. Added comments. #21

--- main.py
+++ main.py
@@ -16,9 +16,13 @@
 from country_lexer import CountryLexer
 from latex_generator import Latex_Generator
 from html_generator import HTML_Generator
+import sys
+
+# Collect user's input filename
+filename = sys.argv[1]
 
 # Instance of class to apply Language Processing
-cl = CountryLexer('small_text.csv')
+cl = CountryLexer(filename)
 # Apply lex processes on file
 cl.process()
 
@@ -26,21 +30,27 @@ print("Choose columns you want to be seen on both documents.\n"
       "Each desired column should be inputted individually, per line.\n"
       "1st column is enabled by default, for visualization purposes.\n"
       "To end the choice of columns, it's sent an space string")
+
 # Show columns present on csv
 for column in cl.countries[0].columns:
     print('-'+ column)
 
-# Read input from user
 
+# -> Columns to be shown
 chosen_columns = []
-    # -> Columns to be shown
+
+    # Read input from user
 for line in iter(lambda: input(">> "), " "):
 
+    # Add columns in case it's present on csv file and it's not the first one
     if line in cl.countries[0].columns[1:]:
         chosen_columns.append(line)
 
+    # Warn user, in case is picked the first column
     elif line == cl.countries[0].columns[0]:
         print("This column is chosen by default and cannot be excluded.")
+
+    # Warn user for insistent columns
     else:
         print("Column doesn't exist")
 
